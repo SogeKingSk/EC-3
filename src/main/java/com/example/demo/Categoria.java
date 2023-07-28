6543a8161d7dae89f9f887a090ffdb1a2deed7c4
SogeKingSk <136747881+SogeKingSk@users.noreply.github.com>
2023-07-28 01:02:46 +0000

    estabien1

---
+++ src/main/java/com/example/demo/Categoria.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    private Integer id;
+    private String nombre;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+}
